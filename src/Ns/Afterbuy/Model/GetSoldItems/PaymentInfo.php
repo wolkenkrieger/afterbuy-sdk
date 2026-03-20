dd7f5202ad0d238f8041c3443bba26d0f9a6a32a
wolkenkrieger <wolkenkrieger@gmail.com>
2026-03-20 13:00:10 +0100

    bugfix annotation error

--- src/Ns/Afterbuy/Model/GetSoldItems/PaymentInfo.php
+++ src/Ns/Afterbuy/Model/GetSoldItems/PaymentInfo.php
@@ -17,14 +17,14 @@ class PaymentInfo extends AbstractPaymentInfo
      * @Serializer\SerializedName("PaymentID")
      * @var string
      */
-    protected $paymentId;
+    protected string $paymentId = '';
 
     /**
      * @Serializer\Type("string")
      * @Serializer\SerializedName("PaymentFunction")
      * @var string
      */
-    protected $paymentFunction;
+    protected string $paymentFunction = '';
 
     /**
      * @Serializer\Type("Ns\Afterbuy\Model\GetSoldItems\PaymentData")
@@ -38,14 +38,14 @@ class PaymentInfo extends AbstractPaymentInfo
      * @Serializer\SerializedName("PaymentTransactionID")
      * @var string
      */
-    protected $paymentTransactionId;
+    protected string $paymentTransactionId = '';
 
     /**
      * @Serializer\Type("string")
      * @Serializer\SerializedName("PaymentStatus")
      * @var string
      */
-    protected $paymentStatus;
+    protected string $paymentStatus = '';
 
     /**
      * @Serializer\Type("Ns\Afterbuy\Model\FloatType")
@@ -59,7 +59,7 @@ class PaymentInfo extends AbstractPaymentInfo
      * @Serializer\SerializedName("PaymentInstruction")
      * @var string
      */
-    protected $paymentInstruction;
+    protected string $paymentInstruction = '';
 
     /**
      * @Serializer\Type("DateTime<'d.m.Y H:i:s', 'UTC', '!d.m.Y'>")
@@ -73,7 +73,7 @@ class PaymentInfo extends AbstractPaymentInfo
      * @Serializer\SerializedName("EFTID")
      * @var string
      */
-    protected $eftid;
+    protected string $eftid = '';
 	
 	/**
 	 * @Serializer\Type("array<Ns\Afterbuy\Model\GetSoldItems\PayoutId>")
